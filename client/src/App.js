6e6fa325767bad992ac69dc9d99a8e9027ad4e57
Yash Solanki <yash_solanki@persistent.com>
2022-05-12 00:45:52 +0530

    implemented search bar
    Signed-off-by: Yash Solanki <yash_solanki@persistent.com>

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+import Navbaar from './components/header/Navbaar';
+function App() {
+  return (
+    <>
+    <Navbaar /> 
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/Navbaar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./navbbar.css"
+
+function Navbaar() {
+  return (
+   <header>
+       <nav>
+           <div className="left">
+            <div className='navlogo'>
+            <img src="./amazon_PNG25.png" alt='' />
+            </div>
+            <div className='nav_Searchbaar'>
+                <input type="text" name="" id="" />
+                <div class="search_icon">
+                    
+                </div>
+            </div>
+                
+           </div>
+           <div className="right">
+
+           </div>
+       </nav>
+   </header>
+  )
+}
+
+export default Navbaar
