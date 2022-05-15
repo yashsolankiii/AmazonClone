13cbb507d6d3f382308ff656efc61be7bc52763d
d-minato <yashsolanki478@gmail.com>
2022-05-15 16:09:39 +0530

    database created
    Signed-off-by: d-minato <yashsolanki478@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 import Navbaar from './components/header/Navbaar';
--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("./db/conn")
 
 const port = 8005;
 
