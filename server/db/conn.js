a6a2a61ef9dd31acc6e2f87c616935a83b0ae98d
d-minato <yashsolanki478@gmail.com>
2022-05-17 15:29:00 +0530

    product schema added
    Signed-off-by: d-minato <yashsolanki478@gmail.com>

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,5 +2,7 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE;
 
-
-mongoose.connect(DB).then(()=>console.log("database connected")).catch((error)=> console.log("error" + error.message))
+mongoose.connect(DB,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}).then(()=>console.log("connection is successfully done")).catch((error)=>console.log("error hai" + error.message))
